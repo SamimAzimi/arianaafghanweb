f99d7d89ed40043b4a78187c7d1a30849517e9b6
Samim.Azimi <samim.elite8@gmail.com>
2022-06-02 17:56:38 +0430

    image modified

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react'
 import Ariana_Afghan_Logo from './assets/Ariana_Afghan_logo.png'
 export default function App() {
   return (
-    <img src={Ariana_Afghan_Logo} alt={Ariana_Afghan_Logo} />
+
+    <div className='logo'>
+
+      <img src={Ariana_Afghan_Logo} alt={Ariana_Afghan_Logo} />
+    </div>
   )
 }
